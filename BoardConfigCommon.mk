7675773d19acc021e2e1042f6dbe5609c763dfd1
Bruno Martins <bgcngm@gmail.com>
2023-01-16 11:48:12 +0800

    sdm660-common: BoardConfig: Sync boot image header with stock
    Pass proper boot image header version and at the same time
    ditch defaults.
    
    Change-Id: If0345731ab8edc9a7f4c2c506e3748833d6dd076

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,6 +34,7 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
 # Kernel
+BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_CMDLINE := \
     androidboot.configfs=true \
     androidboot.hardware=qcom \
@@ -48,6 +49,7 @@ BOARD_KERNEL_CMDLINE := \
     service_locator.enable=1 \
     usbcore.autosuspend=7 \
     user_debug=31
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb

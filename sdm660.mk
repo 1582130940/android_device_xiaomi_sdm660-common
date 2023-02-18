c1399ced53eae509fb5f1ae8d0daa01fb02fa7f0
Alin Jerpelea <alin.jerpelea@sony.com>
2023-02-18 17:26:55 +0800

    sdm660-common: config: Add qmi_fw.conf
    This file will silence the warnings on qmi
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>
    Change-Id: I7d7ec0da00a33a0da8f189b0181d40919b9918e5

--- sdm660.mk
+++ sdm660.mk
@@ -409,6 +409,9 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qmi_fw.conf:$(TARGET_COPY_OUT_VENDOR)/etc/qmi_fw.conf
+
 # RIL
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \

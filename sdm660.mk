58501cdbec99f8def69e65987790fc55bdd5bd56
Max Weffers <rcstar6696@gmail.com>
2023-02-20 18:13:51 +0800

    sdm660-common: device: Add AuthSecret HAL service
    08-20 15:27:45.816  1469  1469 I LockSettingsService: Device doesn't
    implement AuthSecret HAL

--- sdm660.mk
+++ sdm660.mk
@@ -97,6 +97,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_product_strategies.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_product_strategies.xml \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_stream_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_stream_volumes.xml
 
+# AuthSecret HAL
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0-service
+
 # Biometrics
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm660

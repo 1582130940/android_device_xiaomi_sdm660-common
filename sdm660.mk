a1cfecc65515ce65d4d4fbc6e1b60f1c9e6415cb
1582130940 <19632674+1582130940@users.noreply.github.com>
2023-02-27 20:09:46 +0800

    sdm660-common: biometrics: Back to sdm660 fingerprint HIDL

--- sdm660.mk
+++ sdm660.mk
@@ -92,6 +92,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Biometrics
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm660
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
@@ -166,10 +170,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor
-
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 # FM
 ifeq ($(BOARD_HAVE_QCOM_FM),true)

743d6dc778ef35623ea1ab28b65d542552c09ca6
LuK1337 <priv.luk@gmail.com>
2023-03-06 13:35:47 +0100

    sdm660-common: proprietary-files: Sync extractors with templates
    This applies the following tools/extract-utils changes:
    * Allow overriding VENDOR with VENDOR_COMMON
    
    Change-Id: Ib78643a3e6a648e21362b6a1fdc3581d41954700

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,7 +22,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
 write_headers "jasmine_sprout jason lavender platina twolip wayne whyred"
@@ -59,7 +59,7 @@ if [ -s "${MY_DIR}/../$DEVICE_SPECIFIED_COMMON/proprietary-files.txt" ]; then
     DEVICE_COMMON="sdm660-common"
 fi
 
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+if [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
 
@@ -67,7 +67,7 @@ if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     write_headers
 
     # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
+    write_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" true
 
     # Finish
     write_footers

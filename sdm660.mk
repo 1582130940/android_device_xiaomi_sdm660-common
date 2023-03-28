a5d4a2fd586d09f68a771180f27927553bc6b82e
Alcatraz323 <alcatraz32323@gmail.com>
2023-03-28 12:18:11 +0800

    sdm660-common: device: Use QTI USB 1.3 HAL
    * Usb sepolicy defined android_device_qcom_sepolicy lineage-20.0-legacy-um
    * Pull appropriate USB props from vendor_product.mk
    * Remove LineageOS Trust HAL

--- sdm660.mk
+++ sdm660.mk
@@ -510,11 +510,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service-qti
+
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh

9c53dd4ff6e82fb7d3fc9db94118b6ec031d76cc
Ratoriku <a1063021545@gmail.com>
2023-03-28 11:43:20 +0800

    sdm660-common: device: Copy excluded-input-devices.xml from main source
    * Since there are no need for us to do any specific changes here
    
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -491,6 +491,9 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \

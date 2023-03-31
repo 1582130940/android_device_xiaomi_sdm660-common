16e1f8ba64c4277cc25976e45d7c82dc41a05c76
1582130940 <19632674+1582130940@users.noreply.github.com>
2023-03-31 11:26:07 +0800

    sdm660-common: light: Import light from hardware_xiaomi
    * Partial revert: sdm660-common: Move to common Xiaomi lights AIDL
    *
    https://github.com/LineageOS/android_hardware_xiaomi/tree/lineage-20/aidl/light
    * Last commit: "aidl: light: Add "backlight" to the list of backlight
    devices"
    * Rename xiaomi to xiaomi_sdm660

--- sdm660.mk
+++ sdm660.mk
@@ -268,7 +268,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.xiaomi_sdm660
 
 # LiveDisplay
 PRODUCT_PACKAGES += \

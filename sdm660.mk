51f4c53b90b446645a70ecbee45edba1b7a6f470
ghostrider-reborn <adithya.r02@outlook.com>
2023-07-17 17:29:27 +0800

    sdm660-common: device: Optimize ART
    * Disable ART debug and optimize dexpreopt
    * Disable dexpreopt minidebuginfo
    ghostrider-reborn <adithya.r02@outlook.com>

--- sdm660.mk
+++ sdm660.mk
@@ -51,6 +51,14 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
+# ART
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
+USE_DEX2OAT_DEBUG := false
+WITH_DEXPREOPT_DEBUG_INFO := false
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl:32 \

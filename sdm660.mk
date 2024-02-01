a227b2a4c559573d7ded6b0a368e5448d45b8fb7
1582130940 <19632674+1582130940@users.noreply.github.com>
2024-02-01 19:43:54 +0800

    sdm660-common: configs: Adapt powerhint to overclock sdm660 freq
    * Remove powerhint_636.json

--- sdm660.mk
+++ sdm660.mk
@@ -457,7 +457,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint_636.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_636.json \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 PRODUCT_SOONG_NAMESPACES += \

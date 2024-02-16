6519ec9507e9ef4a6b4f332314e28b1047544072
Gabriel Biren <gbiren@google.com>
2024-02-16 21:54:35 +0800

    sdm660-common: device: Update makefile to use the AIDL Wifi Vendor HAL
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- sdm660.mk
+++ sdm660.mk
@@ -684,7 +684,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi.hostapd@1.3.vendor \
     android.hardware.wifi.supplicant-V1-ndk.vendor \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     hostapd_cli \
     libwifi-hal-qcom \

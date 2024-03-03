418f140775f50db4ce1cca20639c878effdd79ff
LuK1337 <priv.luk@gmail.com>
2024-03-04 02:27:37 +0800

    sdm660-common: device: Build android.hardware.bluetooth@1.0
    Required by libantradio.
    
    Change-Id: I5e26a4154cf54e64f424cb6cf20db87b7023f1cd

--- sdm660.mk
+++ sdm660.mk
@@ -137,6 +137,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor

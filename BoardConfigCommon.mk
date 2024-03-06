77e884e7343476aea7f3056fcfc5666d5916ae98
Bruno Martins <bgcngm@gmail.com>
2024-03-06 20:34:40 +0800

    sdm660-common: props: Prefer 'cache' backing storage
    Android 14 allows using /cache as scratch partition. This applies only
    to non-A/B devices and provides much faster overlayfs setup.
    
    Change-Id: Iac5e86a9ca5ecd9d72840e753ea4a58c71079917

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,6 +156,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Releasetools

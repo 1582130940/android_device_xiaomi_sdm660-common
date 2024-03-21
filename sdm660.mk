eda2c1d491efbeab3d55e036212422604e8306e5
basamaryan <basam.aryan@gmail.com>
2024-03-21 13:26:07 +0800

    sdm660-common: device: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- sdm660.mk
+++ sdm660.mk
@@ -8,9 +8,6 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Enable userspace reboot
 $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 

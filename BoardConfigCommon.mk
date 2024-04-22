7c2fb9bd2c71c1eb698b23abcc07f8b131cc95e6
Bruno Martins <bgcngm@gmail.com>
2024-04-22 23:46:50 +0800

    sdm660-common: BoardConfig: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,7 +104,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_sdm660

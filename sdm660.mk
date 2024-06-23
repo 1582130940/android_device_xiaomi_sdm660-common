62785171228cc29f7104e13c04b60f19d1d01beb
Bruno Martins <bgcngm@gmail.com>
2024-06-24 02:53:40 +0800

    sdm660-common: Android: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- sdm660.mk
+++ sdm660.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 

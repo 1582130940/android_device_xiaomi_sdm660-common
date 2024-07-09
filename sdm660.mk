e8efdb78814bfa4af83d0486a307e3e7c1bb4910
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-09 18:55:13 +0800

    sdm660-common: device: Migrate to QTI USB Gadget 1.2 HIDL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- sdm660.mk
+++ sdm660.mk
@@ -662,11 +662,13 @@ PRODUCT_BOOT_JARS += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service-qti \
+    android.hardware.usb.gadget@1.2-service-qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
-    init.qcom.usb.sh
+    init.qcom.usb.sh \
+    usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc

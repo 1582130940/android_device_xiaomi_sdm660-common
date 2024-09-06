b9dc47fd4b1dcbabec2fb7a36e069942d0819744
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-06 10:17:54 +0800

    sdm660-common: device: Move to Xiaomi IR AIDL
    Change-Id: I46094bcbfaba252b5d98ea8211b927fdaa079e2d

--- sdm660.mk
+++ sdm660.mk
@@ -196,7 +196,7 @@ PRODUCT_PACKAGES += \
 # Consumer IR
 ifeq ($(BOARD_HAVE_IR),true)
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi_sdm660
+    android.hardware.ir-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml

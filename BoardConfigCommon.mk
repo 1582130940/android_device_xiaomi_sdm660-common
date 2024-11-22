906466a5d2bcfc6caa38bb63a2d5cb9da90f9c5d
Michael Bestas <mkbestas@lineageos.org>
2024-11-22 14:51:42 +0800

    sdm660-common: BoardConfig: Set androidboot.init_fatal_reboot_target=recovery
    * Don't crash to bootloader, users can access pstore in recovery in order to debug the issue.
    
    Change-Id: I33e91fa31477785ba9c14a56d1c7ee9110fcc0c9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,6 +39,7 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.boot_devices=soc/c0c4000.sdhci \
     androidboot.configfs=true \
     androidboot.hardware=qcom \
+    androidboot.init_fatal_reboot_target=recovery \
     androidboot.usbcontroller=a800000.dwc3 \
     ehci-hcd.park=3 \
     loop.max_part=7 \

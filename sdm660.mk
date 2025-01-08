1fd55277735268b738c1973fbbad2a06ddcf8bce
someone5678 <nemui3353@gmail.com>
2025-01-09 02:21:50 +0800

    sdm660-common: device: Create dummy libldacBT_bco
    * This target is never existed and Bluetooth apex module also doesn't actually use it.
      But module still opening the library. Which cause log spam.
    
    * This commit is not required if you can just edit the source.
    
    a2dp_vendor_ldac_decoder: packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac_decoder.cc:135 A2DP_VendorLoadDecoderLdac: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    
    Change-Id: I8edfd11a7a87c277d01487219650bb30bfa9f46a

--- sdm660.mk
+++ sdm660.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
+    libldacBT_bco \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
